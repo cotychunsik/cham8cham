ab4d43c52eb42620975449767c93ed1164d8d181
cotychunsik <toboldygo@naver.com>
2024-09-29 11:13:36 +0900

    add metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,16 @@
 // src/app/layout.tsx
 import Head from 'next/head';
+import { Metadata } from 'next';
 
 import './globals.css'
 import { ReactNode } from 'react';
 import Navbar from './components/Nav';
 import Footer from './components/Footer';
 
+export const metadata: Metadata = {
+  title: 'SI expert Charm&Cham Co',
+  description: '외주개발, SI, IT아웃소싱, 퍼블리싱 외주, 백엔드 외주',
+};
 export default function RootLayout({
   children,
 }: {
@@ -15,7 +20,9 @@ export default function RootLayout({
     <html lang="en">
               <Head>
               <title>IT 아웃소싱 전문 참참컴퍼니 Charm8Cham Co.</title>
-        <meta name="description" content="Marketing page built with Next.js and Tailwind CSS" />
+        <meta name="참참컴퍼니 SI 개발" content="비대면 외주 전문 IT 아웃소싱 참참컴퍼니" />
+        <meta name="naver-site-verification" content="36004bd753e8793dd78e81c8a54ea173be67d4a0" />
+        <meta name="google-site-verification" content="csLoOqnnkMTWJeRekG7X6lt7eJuuJFHbktPcBHcbd-0" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <body className='min-h-screen flex flex-col overflow-x-hidden'>      
